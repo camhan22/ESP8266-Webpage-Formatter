37e10490a6d5fa7d0bb261e8a7432e3da5981bdb
camhan22 <hansoncameron18@yahoo.com>
2020-08-19 14:41:19 -0400

    Added Files
    Added python program file and updated the readme with an example

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # ESP8266-Webpage-Formatter
- Simple python module to format regular html code into a string for ESP8266 webserver
+Simple python module to format regular html code into a string for ESP8266 webserver
+
+#NOTE: I have not tested this for all cases of html formatting. Please ensure that the converted code matches what you would expect
+To know if it worked properly, all quotation marks inside the html should be escaped with a back slash preceding it. Each line should should start and end with a single quote mark
+
+Start by writing regular html code that can be tested for format and save as .txt when done. Then run the program in a python shell to convert the file to a string that can be copied and pasted into arduino code directly to be used with ESP8266 Webserver
+
+example:
+This was the python prompt and answers that were used to generate the code:
+
+Enter the file name of the file to convert: Main.txt
+Enter the name of the page to be converted: MAIN_page
+
+This is the code contained in Main.txt:
+
+<html>
+	<title>First Web Server</title>
+	<head>
+		<body>
+			<form method = "get">
+			<input type = "submit" name = "led" value = "ON">
+			<input type = "submit" name = "led" value = "OFF">
+			</form>
+		</body>
+	</head>
+</html>
+
+Previous code will be converted to the following for copy and paste:
+
+String MAIN_page =
+"<html>"
+	"<title>First Web Server</title>"
+	"<head>"
+		"<body>"
+			"<form method = \"get\">"
+			"<input type = \"submit\" name = \"led\" value = \"ON\">"
+			"<input type = \"submit\" name = \"led\" value = \"OFF\">"
+			"</form>"
+		"</body>"
+	"</head>"
+"</html>";
---
+++ WebPage_Format.py
@@ -0,0 +1,46 @@
+file_name = input("Enter the file name of the file to convert: ") #Get the file name to work on
+file=open(file_name,'r+') #Open the file
+text = file.read() #Read the string to operate on
+char_num = 0 #Start at the beginning after the first open bracket
+page_name = input("Enter the name of the page to be converted: ")
+
+
+def insert_quote(string, index):
+    return string[:index] + '"' + string[index:]
+
+def insert_slash(string, index):
+    return string[:index] + '\\' + string[index:]
+
+
+while char_num < len(text) - 1: #Go on until reached one before end of file
+    #Add escaping characters first
+    if text[char_num] == '\"':
+        text = insert_slash(text,char_num)
+        char_num = char_num + 1
+
+    #Add quotes to beginning and end now
+    elif text[char_num] == '>' and text[char_num+1] == '\n':
+        text = insert_quote(text,char_num+1)
+        char_num = char_num + 1 #skip one char since we inserted a new one
+
+    elif text[char_num] == '<' and (text[char_num-1] == '\t' or text[char_num-1] == '\n'):
+        text = insert_quote(text,char_num)
+        char_num = char_num + 1 #skip one character or it causes an infinite loop
+
+    #Add the beginning and end quotes to the entire block of text
+    elif char_num == 0 and text[char_num] != '"':
+        text = insert_quote(text,char_num)
+
+    elif char_num == len(text)-2 and text[char_num+1] != '"':
+        text = text + '\"'
+                            
+    char_num = char_num + 1 #Increment the char num
+
+text = "String " + page_name + " =\n" + text + ";"
+
+file.seek(0)
+print(text)
+file.write(text)
+file.close()
+
+input("press enter to quit...")
